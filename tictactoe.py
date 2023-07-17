55906f78dd869dc0c10df5bb609e89951f5116e7
Tomasz Kaczmarek <108423165+TomaszKacz@users.noreply.github.com>
2023-07-17 12:16:58 +0200

    Second Stage Completed

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,13 @@
 # write your code here
+usr_input = input()
+first_row = usr_input[0:3]
+second_row = usr_input[3:6]
+third_row = usr_input[6:9]
+
+
+
+print("---------")
+print("|", *first_row, "|", sep=" ")
+print("|", *second_row, "|", sep=" ")
+print("|", *third_row, "|", sep=" ")
+print("---------")
