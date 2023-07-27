4f09f372fddcbaf10592c7d1315dfc776a9a08c0
Tomasz Kaczmarek <108423165+TomaszKacz@users.noreply.github.com>
2023-07-27 13:18:02 +0200

    Stage 4 - idea

--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
 from test.tests import TicTacToeTest
 
-if __name__ == '__main__':
-    TicTacToeTest().run_tests()
+if __name__ == '__main__':    TicTacToeTest().run_tests()
--- tictactoe.py
+++ tictactoe.py
@@ -13,6 +13,7 @@ def count_letters(game_input):
     difference = abs(countero - counterx)
     return difference, counterspace
 def game_board(first_row, second_row, third_row):
+
     print("---------")
     print("|", *first_row, "|", sep=" ")
     print("|", *second_row, "|", sep=" ")
@@ -28,33 +29,69 @@ second_row = usr_input[3:6]
 third_row = usr_input[6:9]
 diffrence, whitespace = count_letters(usr_input)
 game_board(first_row,second_row,third_row)
-if first_row == "XXX" or second_row == "XXX" or third_row == "XXX":
-    winner_x += 1
-for i in range(0, 3):
-    if first_row[i] == "X" and second_row[i] == "X" and third_row[i] == "X":
-        winner_x += 1
-for i in range(0, 3):
-    if first_row[i] == "O" and second_row[i] == "O" and third_row[i] == "O":
-        winner_o += 1
-if first_row[2] == "X" and second_row[1] == "X" and third_row[0] == "X":
-    winner_x += 1
-if first_row[0] == "X" and second_row[1] == "X" and third_row[2] == "X":
-    winner_x += 1
-if first_row == "OOO" or second_row == "OOO" or third_row == "OOO":
-    winner_o += 1
-if first_row[2] == "O" and second_row[1] == "O" and third_row[0] == "O":
-    winner_o += 1
-if first_row[0] == "O" and second_row[1] == "O" and third_row[2] == "O":
-    winner_o += 1
-if winner_x > 0 and winner_o > 0 or diffrence >= 2:
-    print("Impossible")
-elif winner_x > 0 and winner_o == 0:
-    print("X wins")
-elif winner_o > 0 and winner_x == 0:
-    print("O wins")
-elif whitespace == 0 and winner_o == 0 and winner_x == 0:
-    print("Draw")
-else:
-    print("Game not finished")
+while True:
+    usr_move = input("Where u want to place X ")
+    real_move = usr_move.split()
+    try:
+        for i in range(0 ,len(real_move)):
+            real_move[i] = int(real_move[i])
+    except ValueError:
+        print("You should enter numbers!")
+    else:
+        if int(real_move[0]) > 3 or int(real_move[0]) < 1 or int(real_move[1]) > 3 \
+                or int(real_move[1]) < 1:
+            print("Coordinates should be from 1 to 3!")
+        elif int(real_move[0]) == 1:
+            if first_row[int(real_move[1]) - 1] != " " or \
+                    first_row[int(real_move[1]) - 1] != "_":
+                    print("This cell is occupied! Choose another one!")
+            else:
+                first_row = first_row[:int(real_move[1]) - 1] + "X" + first_row[int(real_move[1]):]
+                game_board(first_row, second_row, third_row)
+                break
+        elif int(real_move[0]) == 2:
+            if second_row[int(real_move[1]) - 1] != " " or \
+                    second_row[int(real_move[1]) - 1] != "_":
+                    print("This cell is occupied! Choose another one!")
+            else:
+                second_row = second_row[:int(real_move[1]) - 1] + "X" + second_row[int(real_move[1]):]
+                game_board(first_row, second_row, third_row)
+                break
+        elif int(real_move[0]) == 3:
+            if third_row[int(real_move[1]) - 1] != " " or \
+                    third_row[int(real_move[1]) - 1] != "_":
+                    print("This cell is occupied! Choose another one!")
+            else:
+                third_row = third_row[:int(real_move[1]) - 1] + "X" + third_row[int(real_move[1]):]
+                game_board(first_row, second_row, third_row)
+                break
+# if first_row == "XXX" or second_row == "XXX" or third_row == "XXX":
+#     winner_x += 1
+# for i in range(0, 3):
+#     if first_row[i] == "X" and second_row[i] == "X" and third_row[i] == "X":
+#         winner_x += 1
+# for i in range(0, 3):
+#     if first_row[i] == "O" and second_row[i] == "O" and third_row[i] == "O":
+#         winner_o += 1
+# if first_row[2] == "X" and second_row[1] == "X" and third_row[0] == "X":
+#     winner_x += 1
+# if first_row[0] == "X" and second_row[1] == "X" and third_row[2] == "X":
+#     winner_x += 1
+# if first_row == "OOO" or second_row == "OOO" or third_row == "OOO":
+#     winner_o += 1
+# if first_row[2] == "O" and second_row[1] == "O" and third_row[0] == "O":
+#     winner_o += 1
+# if first_row[0] == "O" and second_row[1] == "O" and third_row[2] == "O":
+#     winner_o += 1
+# if winner_x > 0 and winner_o > 0 or diffrence >= 2:
+#     print("Impossible")
+# elif winner_x > 0 and winner_o == 0:
+#     print("X wins")
+# elif winner_o > 0 and winner_x == 0:
+#     print("O wins")
+# elif whitespace == 0 and winner_o == 0 and winner_x == 0:
+#     print("Draw")
+# else:
+#     print("Game not finished")
 
 
